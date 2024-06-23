41daf2678c7574849bd27e0d6a25026caff31ddc
Deepak <deepaknahak206@gmail.com>
2024-06-23 22:47:32 +0530

    2nd

---
+++ src/main/java/com/deepak/calling_3rdpartyapi/Repo/Category.java
@@ -0,0 +1,6 @@
+package com.deepak.calling_3rdpartyapi.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Category extends JpaRepository<Category,Long> {
+}
---
+++ src/main/java/com/deepak/calling_3rdpartyapi/Repo/ProductRepo.java
@@ -0,0 +1,6 @@
+package com.deepak.calling_3rdpartyapi.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<ProductRepo,Long> {
+}
